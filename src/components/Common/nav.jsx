43ad2bbbb04c102ada11e481529e6ea0dc1544d0
songah <skutcm119@gmail.com>
2022-08-20 14:13:34 +0900

    chore: nav component set

--- src/components/Common/nav.jsx
+++ src/components/Common/nav.jsx
@@ -3,15 +3,35 @@ import styled from "styled-components";
 const Nav = () => {
   return (
     <StNav>
-      <StIconBtn>
-        <img src="" alt="" />
-      </StIconBtn>
+      <StIconWrapper>
+        <StIconBtn>
+          <img src="" alt="홈" />
+        </StIconBtn>
+        <StIconBtn>
+          <img src="" alt="" />
+        </StIconBtn>
+        <StIconBtn>
+          <img src="" alt="마이페이지" />
+        </StIconBtn>
+        <StIconBtn>
+          <img src="" alt="설정" />
+        </StIconBtn>
+      </StIconWrapper>
     </StNav>
   );
 };
 
 export default Nav;
-const StNav = styled.div``;
+const StNav = styled.div`
+  width: 375px;
+
+  background-color: white;
+`;
+const StIconWrapper = styled.div`
+  display: flex;
+
+  margin-top: 3px;
+`;
 const StIconBtn = styled.div`
   & > img {
     width: 20px;
