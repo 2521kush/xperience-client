127e473d2257cd725faf3600ef7db6f77efd0a34
YoungJinnn <yjbulee@postech.ac.kr>
2022-08-21 00:27:57 +0900

    chore: git pull

--- src/pages/Junior/JuniorSetting.jsx
+++ src/pages/Junior/JuniorSetting.jsx
@@ -1,4 +1,14 @@
+import Setting from "assets/Setting.png";
+import Top from "components/common/Top";
+
 const JuniorSetting = () => {
-  return <></>;
+  return (
+  <div className = 'setting_container'>
+    <Top/>
+    <div className = "setting_text">나희도님, 안녕하세요</div>
+    <br></br><br></br>
+    <img className="setting_img" src={Setting} alt="React" />
+  </div>
+  )
 };
 export default JuniorSetting;
