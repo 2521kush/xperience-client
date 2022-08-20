ef20f4c709590782ab178cf0ec0941c03c40466f
songah <skutcm119@gmail.com>
2022-08-20 16:39:10 +0900

    design: 경로에 따른 조건부 스타일

--- src/components/Common/nav.jsx
+++ src/components/Common/nav.jsx
@@ -1,20 +1,61 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import activeHome from "assets/nav_home_active.png";
+import inactiveHome from "assets/nav_home_inactive.png";
+import activeSchedule from "assets/nav_schedule_active.png";
+import inactiveSchedule from "assets/nav_schedule_inactive.png";
+import activeProfile from "assets/nav_profile_active.png";
+import inactiveProfile from "assets/nav_profile_inactive.png";
+import activeSetting from "assets/nav_setting_active.png";
+import inactiveSetting from "assets/nav_setting_inactive.png";
 
 const Nav = () => {
+  const router = useLocation();
+  const [currentPath, setcurrentPath] = useState("home");
+
+  const getCurrentPath = () => {
+    if (router.pathname.includes("/schedule")) {
+      setcurrentPath("schedule");
+    } else if (router.pathname.includes("/mypage")) {
+      setcurrentPath("profile");
+    } else if (router.pathname.includes("/setting")) {
+      setcurrentPath("setting");
+    } else {
+      setcurrentPath("home");
+    }
+  };
+
+  useEffect(() => {
+    getCurrentPath();
+  }, []);
+
   return (
     <StNav>
       <StIconWrapper>
         <StIconBtn>
-          <img src="" alt="홈" />
+          <img
+            src={currentPath === "home" ? activeHome : inactiveHome}
+            alt="홈"
+          />
         </StIconBtn>
         <StIconBtn>
-          <img src="" alt="" />
+          <img
+            src={currentPath === "schedule" ? activeSchedule : inactiveSchedule}
+            alt="일정"
+          />
         </StIconBtn>
         <StIconBtn>
-          <img src="" alt="마이페이지" />
+          <img
+            src={currentPath === "profile" ? activeProfile : inactiveProfile}
+            alt="프로필"
+          />
         </StIconBtn>
         <StIconBtn>
-          <img src="" alt="설정" />
+          <img
+            src={currentPath === "setting" ? activeSetting : inactiveSetting}
+            alt="설정"
+          />
         </StIconBtn>
       </StIconWrapper>
     </StNav>
@@ -22,19 +63,28 @@ const Nav = () => {
 };
 
 export default Nav;
-const StNav = styled.div`
-  width: 375px;
 
-  background-color: yellow;
+const StNav = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 375px;
+  hegiht: 73px;
+
+  top: 694px;
+
+  background-color: white;
 `;
 const StIconWrapper = styled.div`
   display: flex;
 
-  margin-top: 3px;
+  gap: 60px;
+  margin: 12px;
 `;
 const StIconBtn = styled.div`
   & > img {
-    width: 20px;
-    height: 20px;
+    width: 22px;
+    height: 42px;
+    border-radius: 0px;
   }
 `;
